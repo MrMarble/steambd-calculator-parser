f1da00ec91ae9a7005734ab11eaede69c0edf2ee
Alvaro Tinoco <atinoco@hiberus.com>
2019-09-04 12:05:08 +0200

    [Fix] Flake8 F632

--- steamdbparser/SteamDbParser.py
+++ steamdbparser/SteamDbParser.py
@@ -72,7 +72,7 @@ class parser(object):
             logging.info(f'Requesting {steamDBUrl}')
             r = requests.get(
                 steamDBUrl, headers=self.__headers, timeout=(3, 10))
-            if r.status_code is 200:
+            if r.status_code == 200:
                 logging.info('Request completed')
                 soup = BeautifulSoup(r.text, 'html.parser')
 
@@ -182,7 +182,7 @@ class parser(object):
                 try:
                     vanity_url = header.select_one(
                         'div.body-content > .container .tab-content #info > div:first-of-type .span6:first-child table tr:first-child .span2')
-                    if vanity_url and vanity_url.string is 'Vanity URL':
+                    if vanity_url and vanity_url.string == 'Vanity URL':
                         profile['vanity_url'] = header.select_one(
                             'div.body-content > .container .tab-content #info > div:first-of-type .span6:first-child table tr:first-child a')[
                             'href']
