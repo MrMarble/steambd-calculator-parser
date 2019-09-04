bbe933919172b995135284b5ae191851cefe2296
Alvaro Tinoco <atinoco@hiberus.com>
2019-09-04 12:13:53 +0200

    [Fix] expected str

--- tests/test_steamdbparser.py
+++ tests/test_steamdbparser.py
@@ -13,7 +13,7 @@ def test_issteamid():
     assert steam.isSteamId(76561198287455504) is True #My SteamID
     assert steam.isSteamId('76561198287455504') is True
 
-    assert steam.isSteamId(''.join(random.randint(0,9) for i in range(17))) is True
+    assert steam.isSteamId(''.join(str(random.randint(0,9)) for i in range(17))) is True
 
     assert steam.isSteamId(1) is False
     for i in range(10):
